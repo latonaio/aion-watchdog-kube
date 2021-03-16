8ffbcdcdaaa84c42bf95162052c58cb550746b0d
Toyoaki Doi <kokorononakaniitumo@yahoo.co.jp>
2021-03-16 13:00:45 +0900

    release

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"aion-watchdog-kube/config"
 	"context"
 	"k8s.io/client-go/rest"
 	"log"
@@ -26,6 +27,10 @@ func main() {
 	quiteCh := make(chan syscall.Signal, 1)
 
 	ctx, cancel := context.WithCancel(context.Background())
+	cfg,err := config.New()
+	if err != nil {
+		errCh <- err
+	}
 
 	c, err := msclient.NewKanbanClient(ctx)
 	if err != nil {
@@ -37,17 +42,17 @@ func main() {
 		errCh <- err
 	}
 
-	config, err := rest.InClusterConfig()
+	kubecfg, err := rest.InClusterConfig()
 	if err != nil {
 		errCh <- err
 	}
 
-	clientset, err := kubernetes.NewForConfig(config)
+	clientset, err := kubernetes.NewForConfig(kubecfg)
 	if err != nil {
 		errCh <- err
 	}
 
-	go watch(ctx, clientset, errCh, c)
+	go watch(ctx, clientset, errCh, c,cfg)
 
 loop:
 	for {
@@ -61,8 +66,8 @@ loop:
 	}
 }
 
-func watch(ctx context.Context, clientset *kubernetes.Clientset, errCh chan error, c msclient.MicroserviceClient) {
-	ticker := time.NewTicker(2 * time.Second)
+func watch(ctx context.Context, clientset *kubernetes.Clientset, errCh chan error, c msclient.MicroserviceClient, cfg *config.Config) {
+	ticker := time.NewTicker(time.Duration(cfg.WatchPeriod) * time.Second)
 	defer ticker.Stop()
 	for {
 		select {
@@ -71,18 +76,16 @@ func watch(ctx context.Context, clientset *kubernetes.Clientset, errCh chan erro
 		case <-ticker.C:
 			pods, err := clientset.CoreV1().Pods("").List(ctx, metav1.ListOptions{})
 			if err != nil {
-				log.Fatalln("failed to get pods:", err)
+				errCh <- err
+				return
 			}
 
 			for _, pod := range pods.Items {
-				log.Printf(" pod name: %+v",pod.Name)
-				log.Printf("%+v",pod.Status.ContainerStatuses[0].Name)
 				for _,status := range pod.Status.ContainerStatuses {
-					log.Printf("status %+v",status.Name)
 					if status.Ready == false {
 						if hasWaitingStatusProblem(status.State.Waiting.Reason) &&
-							countList[status.Name] <= 3 {
-							log.Printf("起動に失敗したpodを検知しました。POD名: %s, Reason: %s", pod.Name, pod.Status.ContainerStatuses[0].State.Waiting.Reason)
+							countList[status.Name] <= cfg.MaxAlertNum-1 {
+							log.Printf("起動に失敗したpodを検知しました。POD名: %s, Reason: %s", pod.Name, status.State.Waiting.Reason)
 							ck := msclient.SetConnectionKey("slack")
 							metadata := msclient.SetMetadata(map[string]interface{}{
 								"pod_name": pod.Name,
